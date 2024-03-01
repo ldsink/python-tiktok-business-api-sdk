87362cec757eb13d554b38ee5dbde4ac070fa70b
ZHOU Cheng <c.zhou@live.com>
2024-03-01 16:40:29 +0800

    rename package name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 """
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "python_sdk"
+NAME = "tiktok_business_api_sdk"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
