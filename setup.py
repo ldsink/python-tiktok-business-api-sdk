2dcfa8ab0f89957624a402825d1f684487e701ca
ᴀᴍᴛᴏᴀᴇʀ <amtoaer@outlook.com>
2024-03-07 11:53:51 +0800

    chore: 加入 __init__.py (#1)
    * chore: 加入 __init__.py
    
    * chore: 修改版本号

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "tiktok_business_api_sdk"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 # To install the library, run the following
 #
 # python setup.py install
